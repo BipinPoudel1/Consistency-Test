121b36d27ad830953406d3fa44f9ea7516eb278b
Bipin <xperiabpn@gmail.com>
2022-12-06 22:44:28 +0545

    2. Escape Sequence, Comment, Separator and End

---
+++ commentsESC.py
@@ -0,0 +1,36 @@
+'''
+      This is a
+      multiline
+      comment
+'''
+
+"""
+      This is also
+      a 
+      multi- line
+      comment
+"""
+
+print("Hello World!")
+
+
+#Escape Sequence
+print("Hey I am a good boy and\nso are you.")   #new line \n
+
+print("Hey I am a \"Good Boy\" and so are you.") #double quote esc seq.
+
+print('Hey I am a \'Good Boy\' and so are you.')      #single quote esc seq.
+
+
+#Separator
+'''We can separate the values inside the print statement using sep="character that separates"'''
+#example
+
+print("Hey", "Let's", "Have", "some", "coffee", sep="~")
+
+#end
+print("Hey", 6, 7, end="009\n")
+print("Bipin")
+
+print("bipin", end="@")
+print("poudel")
