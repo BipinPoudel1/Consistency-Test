847dd4585b40589a1a0d432e35569a7ad6576a18
Bipin <xperiabpn@gmail.com>
2022-12-07 14:57:05 +0545

    Basic Operators

---
+++ operators.py
@@ -0,0 +1,8 @@
+#basic operators
+print(15+6) #Add
+print(15-6) #Subtract
+print(15*6) #Multiply
+print(15/6) #Divide
+print(15//6)      #Floor division
+print(15%6)       #Modulo
+print(5**3)       #Exponential
